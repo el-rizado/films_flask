63fbec2e5f536db8ac1ccc3af89a50740f9db896
el-rizado <amirhosseinmohseni1996@yahoo.com>
2020-12-06 23:52:08 +0330

    edit app.py to set and get cookies

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 import psycopg2
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def login():
     """ This is the main page of the file """
-    render_template('login.html')
+    return render_template('login.html')
 
 
-@app.route('/films')
+@app.route('/profile', methods=['POST', 'GET'])
+def profile_logged():
+    if request.method == 'POST':
+        username = request.form['uname']
+        response = make_response(render_template('profile.html', username=username))
+        response.set_cookie('userID', username)
+        return response
+    else:
+        # handle GET request.
+        pass
+
+
+# @app.route('/getcookie')
+def get_cookie():
+    username = request.cookies.get('userID')
+    return username
+
+
+@app.route('/profile/films')
 def show_films():
-    return 'films'
+    username = get_cookie()
+    return f'hello {username}'
+    # return 'films'
 
 
-@app.route('/actors')
+@app.route('/profile/actors')
 def show_actors():
+    username = get_cookie()
     return 'actors'
 
 
-@app.route('/films/<int:film_id>')
+@app.route('/profile/films/<int:film_id>')
 def film_info(film_id):
+    username = get_cookie()
     return 'film information'
 
 
