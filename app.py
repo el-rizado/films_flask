e20ee68851cfb6f9f8f8fb8db2f73b7b109b2bf2
el-rizado <amirhosseinmohseni1996@yahoo.com>
2020-12-06 20:24:23 +0330

    edit app.py and add more functions

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
+def login():
     """ This is the main page of the file """
-    return 'hello'
+    render_template('login.html')
+
+
+@app.route('/films')
+def show_films():
+    return 'films'
+
+
+@app.route('/actors')
+def show_actors():
+    return 'actors'
+
+
+@app.route('/films/<int:film_id>')
+def film_info(film_id):
+    return 'film information'
+
+
+if "__name__" == "__main__":
+    app.run("0.0.0.0", 5000)
